4e378852f97b6021f793ec80bcf5401b5c02b7d9
Raiden <rodriguezdazae@gmail.com>
2025-08-29 07:26:16 -0500

    adding readme, chaniging syntax

--- ejemplo1/main.cpp
+++ ejemplo1/main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(int argc, char** argv ){
-        printf("jdfjdfHello World.\n" );
+        printf("jdfjdfHello fvcvcvWorld.\n" );
         return 0;
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+ # asle g2
+ repositorio asignatura alse para aprender c++ y p00
